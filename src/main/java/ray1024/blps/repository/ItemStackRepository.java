0093b836347cba1060f0adcc443a161cf9d6c9eb
Ray_1024 <skovorodnikovda@mail.ru>
2024-09-26 17:24:17 +0300

    Fix unused requests and responses

--- src/main/java/ray1024/blps/repository/ItemStackRepository.java
+++
@@ -1,9 +0,0 @@
-package ray1024.blps.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ray1024.blps.model.entity.ItemStack;
-
-@Repository
-public interface ItemStackRepository extends JpaRepository<ItemStack, Long> {
-}
